b07a413f9be93f507e9fcb79548c126a6e30a0eb
Halit Batuhan ASLAN <halitbatuhanaslan@gmail.com>
2024-11-21 21:42:30 +0300

    Comments and deleting on cell files

--- Homeworks/Homewrok1(Spreadsheet)/Cell.h
+++ Homeworks/Homewrok1(Spreadsheet)/Cell.h
@@ -5,44 +5,65 @@
 #include<string>
 using namespace std;
 
+// Enum to define the type of data a Cell can hold
 enum data_type
 {
-    Number,
-    Formula,
-    str_type
+    Number,     // Represents a numeric value
+    Formula,    // Represents a formula
+    str_type    // Represents a string
 };
 
-
+// Class representing a cell, which can store different types of data
 class Cell
 {
     public:
+        // Sets the data type of the cell
         void set_dt();
+        
+        // Gets the current data type of the cell
         data_type get_dt();
 
+        // Returns the content of the cell as a string
         string getCell();
+        
+        // Sets the content of the cell as a string
         void setCell(string entered);
-
-
-        /*friend bool is_num(const string& str);
-        friend bool is_formula(const string& str);*/
+        
+        // Checks if the cell contains a numeric value
         bool is_num();
+        
+        // Checks if the cell contains a formula
         bool is_formula();
+        
+        // Sets a numeric value to the cell
         void setNum(double new_num);
+        
+        // Returns the numeric value stored in the cell
         double getNum();
 
-        friend ostream& operator<<(ostream& out,Cell &tmp);
+        // Overloads the << operator to allow easy printing of a Cell object
+        friend ostream& operator<<(ostream& out, Cell &tmp);
+
+        // Destructor for the Cell class
         ~Cell();
+
     private:
+        // Variable to store the data type of the cell
         data_type dt;
+        
+        // Stores the formula in the cell (if applicable)
         string formula;
+        
+        // Stores the string value in the cell (if applicable)
         string str;
+        
+        // Stores the numeric value in the cell (if applicable)
         double number;
+        
+        // Stores the raw content of the cell
         string inner_cell;
-
 };
-/*bool is_num(const string& str);
-bool is_formula(const string& str);*/
-
-ostream& operator<<(ostream& out,Cell &tmp);
+// Declaration of the overloaded << operator outside the class
+ostream& operator<<(ostream& out, Cell &tmp);
 
 #endif
--- Homeworks/Homewrok1(Spreadsheet)/cell.cpp
+++ Homeworks/Homewrok1(Spreadsheet)/cell.cpp
@@ -1,125 +1,95 @@
-#include"Cell.h"
-#include<cstdlib>
-#include<string>
-#include<sstream>
-#include<iostream>
+#include "Cell.h"
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <iostream>
 using namespace std;
 
+// Returns the content of the cell as a string
 string Cell::getCell()
-{
-    return inner_cell;
-}
+{return inner_cell;}
 
+// Helper function to check if a string represents a valid number
 bool isValidNumber(const string& str)
 {
     try
     {
-        std::stod(str);
-        return true;
+        std::stod(str); // Attempt to convert the string to a double
+        return true;    // Conversion successful
     }
-    catch (const std::invalid_argument&)
+    catch (const invalid_argument&)
     {
-        return false;
+        return false;   // String is not a valid number
     }
-    catch (const std::out_of_range&)
+    catch (const out_of_range&)
     {
-        return false;
+        return false;   // Number is out of range
     }
 }
 
-
-void Cell::setCell(std::string entered)
-{
-    inner_cell = entered;
-    set_dt();
-    if (get_dt() == Formula)
-        formula = entered;
-    else if (get_dt() == str_type)
-        str = entered;
-    else if (isValidNumber(entered))
-        number = std::stod(entered);
-    else
-        str = entered;  // If conversion fails, treat as a string
-}
-
-/*
+// Sets the content of the cell and determines its data type
 void Cell::setCell(string entered)
 {
-    inner_cell = entered;
-    set_dt();
-    if(get_dt() == Formula)
-        formula = entered;
-    else if(get_dt() == str_type)
-        str = entered;
-    else
-    {
-        number = stod(entered);
-    }
-}*/
+    inner_cell = entered; // Store the raw content
+    set_dt();             // Determine the data type
 
-ostream& operator<<(ostream& out,Cell &tmp)
+    if (get_dt() == Formula)
+        formula = entered;    // Store as formula if applicable
+    else if (get_dt() == str_type)
+        str = entered;        // Store as string if applicable
+    else if (isValidNumber(entered))
+        number = stod(entered); // Convert and store as number
+    else
+        str = entered;        // Default to storing as string
+}
+
+// Overloads the << operator to print the content of a cell
+ostream& operator<<(ostream& out, Cell &tmp)
 {
-    out << tmp.getCell();
+    out << tmp.getCell(); // Output the raw content
     return out;
 }
 
+// Determines the data type of the cell based on its content
 void Cell::set_dt()
 {
-    if(is_formula())
-        dt = Formula;
-    else if(is_num())
+    if (is_formula())
+        dt = Formula;         // Set as formula if it starts with '='
+    else if (is_num())
     {    
-        /*last changes happened here*/
-        dt = Number;
+        dt = Number;          // Set as number if it is numeric
         double num = stod(getCell());
-        setNum(num);
-        /*string str = getCell();
-        double new_num = stod(str);
-        setNum(new_num);*/
+        setNum(num);          // Store the numeric value
     }
     else
-        dt = str_type;
+        dt = str_type;        // Otherwise, set as string type
 }
-data_type Cell::get_dt(){return dt;}
 
+// Returns the current data type of the cell
+data_type Cell::get_dt()
+{return dt;}
 
+// Checks if the cell contains a valid numeric value
 bool Cell::is_num()
 {
     char* end = nullptr;
-    std::strtod(inner_cell.c_str(), &end);
-    return (end == inner_cell.c_str() + inner_cell.size());
+    strtod(inner_cell.c_str(), &end); // Convert to double
+    return (end == inner_cell.c_str() + inner_cell.size()); // Check if conversion used the entire string
 }
+
+// Checks if the cell contains a formula (starts with '=')
 bool Cell::is_formula()
 {
-    return(inner_cell[0] == '=');
+    return (inner_cell[0] == '='); // Returns true if first character is '='
 }
 
+// Sets the numeric value for the cell
 void Cell::setNum(double new_num)
-{
-    number = new_num;
-}
+{number = new_num;}
 
+// Returns the numeric value stored in the cell
 double Cell::getNum()
-{
-    return number;
-}
+{return number;}
 
-Cell::~Cell(){}
-
-
-
-
-
-
-
-/*
-bool is_num(const string& str)
-{
-    char* end = nullptr;
-    std::strtod(str.c_str(), &end);
-    return (end == str.c_str() + str.size());
-}
-bool is_formula(const string& str)
-{
-    return(str[0] == '=');
-}*/
+// Destructor for the Cell class
+Cell::~Cell() {}
